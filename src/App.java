f81ec8cd2e60988e843e8b1f34c490bea911ac15
Andreh Silveira <andrehbahia@gmail.com>
2023-05-28 17:57:39 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeros[]=new int[5];
+        for(int i = 0; i <numeros.length; i++){
+            numeros[i]=i+1;
+            System.out.println(numeros[i]);
+
+        }
+
+    }
+}
