68946d09fea9840cf908cadc588b6f9a807cf149
Andreh Silveira <andrehbahia@gmail.com>
2023-05-29 06:14:13 -0300

    impArrauGerJAVA

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         int numeros[]=new int[5];
         for(int i = 0; i <numeros.length; i++){
             numeros[i]=i+1;
             System.out.println(numeros[i]);
-
         }
+        String arrayString = Arrays.toString(numeros);
+        System.out.println(arrayString);
 
     }
 }
